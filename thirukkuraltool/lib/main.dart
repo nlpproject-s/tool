47ae67d7260074576ac6897e5afa38b5f301988d
kavirajmetech <kavirajmetech@gmail.com>
2024-10-21 00:23:11 +0530

    122221

--- thirukkuraltool/lib/main.dart
+++ thirukkuraltool/lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:thirukkuraltool/landingpage.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'Pages/authentication/login.dart';
-import 'landingpage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,24 +11,134 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return HomePage();
-            } else {
-              return SignInPage();
-            }
-            // return VideosPage();
-            // return ChatbotPage();
-            //return HomeScreen();
-            //return QuizPage();
-          },
-        ));
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            _checkAndShowWordOfTheDayDialog(context);
+          });
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return SignInPage();
+          }
+        },
+      ),
+    );
+  }
+
+  Future<void> _checkAndShowWordOfTheDayDialog(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool hasShownDialog = prefs.getBool('shownWordOfTheDay') ?? false;
+
+    if (!hasShownDialog) {
+      _showWordOfTheDayDialog(context);
+      await prefs.setBool('shownWordOfTheDay', true);
+    }
+  }
+
+  void _showWordOfTheDayDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          contentPadding:
+              EdgeInsets.zero, // No padding to accommodate the image fully
+          content: Stack(
+            children: [
+              Container(
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: AssetImage('assets/image.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Word of the Day",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "Thirukkural",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "Explanation of the word in this context.",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: IconButton(
+                  icon: Icon(Icons.cancel, color: Colors.white),
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Cancel button closes dialog
+                  },
+                  child: Text(
+                    "Cancel",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.favorite_border, color: Colors.white),
+                  onPressed: () {
+                    // Handle "Like" functionality
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
   }
 }
