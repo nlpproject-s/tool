81148dcda5a545e7538d4341defa7d7957448b63
kavirajmetech <kavirajmetech@gmail.com>
2024-10-21 07:56:32 +0530

    75621

--- thirukkuraltool/lib/main.dart
+++ thirukkuraltool/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:thirukkuraltool/landingpage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
 import 'package:shared_preferences/shared_preferences.dart';
 import 'Pages/authentication/login.dart';
 
@@ -17,6 +18,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int val = 0;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,9 +28,10 @@ class _MyAppState extends State<MyApp> {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            _checkAndShowWordOfTheDayDialog(context);
-          });
+          if (val == 0) {
+            _showWordOfTheDayDialog(context);
+            val += 1;
+          }
           if (snapshot.hasData) {
             return HomePage();
           } else {
@@ -37,108 +42,118 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  Future<void> _checkAndShowWordOfTheDayDialog(BuildContext context) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool hasShownDialog = prefs.getBool('shownWordOfTheDay') ?? false;
+  Future<void> _showWordOfTheDayDialog(BuildContext context) async {
+    try {
+      // Fetching word from Firestore
+      // DocumentSnapshot<Map<String, dynamic>> snapshot =
+      //     await FirebaseFirestore.instance
+      //         .collection('Word')
+      //         .doc('1')
+      //         .get();
 
-    if (!hasShownDialog) {
-      _showWordOfTheDayDialog(context);
-      await prefs.setBool('shownWordOfTheDay', true);
-    }
-  }
+      String kural = 'agaram';
+      String explanation = 'first letter';
 
-  void _showWordOfTheDayDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          contentPadding:
-              EdgeInsets.zero, // No padding to accommodate the image fully
-          content: Stack(
-            children: [
-              Container(
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage('assets/image.png'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Word of the Day",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        "Thirukkural",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        "Explanation of the word in this context.",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white.withOpacity(0.8),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 0,
-                right: 0,
-                child: IconButton(
-                  icon: Icon(Icons.cancel, color: Colors.white),
-                  onPressed: () {
-                    Navigator.of(context).pop(); // Close the dialog
-                  },
-                ),
-              ),
-            ],
-          ),
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // if (snapshot.exists) {
+      //   // Extract data if exists
+      //   kural = snapshot.data()?['kural'] ?? 'Kural not found';
+      //   explanation =
+      //       snapshot.data()?['explanation'] ?? 'Explanation not found';
+      // }
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            contentPadding: EdgeInsets.zero,
+            content: Stack(
               children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(); // Cancel button closes dialog
-                  },
-                  child: Text(
-                    "Cancel",
-                    style: TextStyle(color: Colors.white),
+                Container(
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage('assets/image.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Word of the Day",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          kural,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          explanation,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white.withOpacity(0.8),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.favorite_border, color: Colors.white),
-                  onPressed: () {
-                    // Handle "Like" functionality
-                    Navigator.of(context).pop();
-                  },
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                    icon: Icon(Icons.cancel, color: Colors.white),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
                 ),
               ],
             ),
-          ],
-        );
-      },
-    );
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pop(); 
+                    },
+                    child: Text(
+                      "Cancel",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.favorite_border, color: Colors.white),
+                    onPressed: () {
+                      Color:Colors.red,
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
+      );
+    } catch (e) {
+      print("Error fetching Word of the Day: $e");
+    }
   }
 }
